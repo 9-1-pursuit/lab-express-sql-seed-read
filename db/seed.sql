0f0a0d18c80e369107bcc704b82f67b0b5fb69a1
Salinamalek <salinamalek@pursuit.org>
2023-01-17 14:57:31 -0500

    part 1 done

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+\c tuner;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Can You Feel My Heart', 'Bring Me The Horizon', 'Sempiternal', '3:48', true),
+('Body Language', 'Jesse McCartney ft. T-Pain', 'Jesse McCartney ft. T-Pain', '3:40', true),
+('Woman', 'Doja Cat', 'Planet Her', '3:13', false);
