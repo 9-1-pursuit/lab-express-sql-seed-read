67b8e9108ee3248c208ab0c15594628a6d332fdc
Richie-Pursuit <richiecarrasco@pursuit.org>
2023-01-18 01:49:20 -0500

    finished pt 1

---
+++ app.js
@@ -0,0 +1,27 @@
+// Dependencies
+const cors = require('cors');
+const express = require('express');
+const songsController = require('./controllers/songController');
+
+// Congfig
+const app = express();
+
+// Middleware
+
+app.use(cors());
+app.use(express.json());
+app.use('/songs', songsController);
+
+// Routes
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Songs App');
+});
+
+// 404
+app.get('*', (req, res) => {
+  res.status(404).send('Page not found');
+});
+
+// Export
+module.exports = app;
