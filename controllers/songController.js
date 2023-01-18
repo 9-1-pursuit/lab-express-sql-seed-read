ff26699e351e0bd09e2ae5dcb7b6c5cf7730417a
Salinamalek <salinamalek@pursuit.org>
2023-01-18 12:55:26 -0500

    part 2 done

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs");
+const { getAllSongs, getSong, createSong } = require("../queries/songs");
+const {
+  checkName,
+  checkArtist,
+  checkBoolean,
+} = require("../validations/checkSongs.js");
 
 // INDEX
 songs.get("/", async (req, res) => {
@@ -9,6 +14,27 @@ songs.get("/", async (req, res) => {
     res.status(200).json(allSongs);
   } else {
     res.status(500).json({ error: "server error" });
+  }
+});
+
+// SHOW
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await getSong(id);
+  if (!song.message) {
+    res.json(song);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+// CREATE
+songs.post("/", checkName, checkArtist, checkBoolean, async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    res.json(song);
+  } catch (error) {
+    res.status(400).json({ error: error });
   }
 });
 
