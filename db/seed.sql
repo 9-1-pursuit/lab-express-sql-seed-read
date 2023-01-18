5e777071faeb9c716b60d7d576dd68e35c7af7e8
Lennie Nurse <lnursej@gmail.com>
2023-01-18 12:41:58 -0500

    "Update seed"

--- db/seed.sql
+++ db/seed.sql
@@ -4,7 +4,7 @@ INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
 ('Billy Jean', 'Micheal Jackson', 'Thriller', '4:55', true),
 ('Get Money', 'Notorious BIG', 'Junior MAFIA', '4:34', true),
 ('Free Mind', 'Tems ', 'For Broken Ears', '2:20', true),
-('Free', 'Carson', 'NO ', '1:55', true),
+('Free', 'Carson', 'NO ', '1:55', false),
 ('Dead Presidents', 'Jay-z', 'Reasonable Doubt', '4:26', true),
 ('All I NEED', 'Popcaan', 'Fixtape', '2:58', true),
 ('Count Me Out', 'kendrick Lamar', 'Mr. Morale & The Big Steppers', '4:43', true);
