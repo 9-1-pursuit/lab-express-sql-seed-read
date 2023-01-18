9eba946e0beab74503ea14ac4fc7bf7f54d5309f
melanieinpursuit <melaniegonzalez@pursuit.org>
2023-01-18 01:33:40 -0500

    first commit

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+\c tuner;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES 
+('Fireflies', 'Owl City', 'Ocean Eyes (Deluxe Edition)', '3:48', true),
+('Jane', 'Jefferson Starship', 'Freedom at Point Zero', '4:00', true),
+('Prodigal Daughter', 'LIGHTS', 'PEP', '2:55', false),
+('Part II', 'Paramore', 'Paramore (Deluxe Edition)', '4:41', false),
+('blackpill', 'Kala', 'blackpill', '3:11', true);
