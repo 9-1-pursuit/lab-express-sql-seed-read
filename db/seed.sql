d31bdde1ed36776e9e05ba492820c6955d0b348b
Lennie Nurse <lnursej@gmail.com>
2023-01-18 11:48:10 -0500

    "update"

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,6 @@
 \c songs_dev;
 
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
-('Billy Jean', 'Micheal Jackson', 'Bad', '1980', true);
+('Billy Jean', 'Micheal Jackson', 'Thriller', '4:55', true),
+('Get Money', 'Notorious BIG', 'Junior MAFIA', '4:34', true);
+('Boy joy', 'Micheal ', 'Bid', '2:20', false);
