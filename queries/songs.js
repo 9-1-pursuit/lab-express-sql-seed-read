93df1805febe26b287d550b3b8883faadef16b0c
Lennie Nurse <lnursej@gmail.com>
2023-01-18 10:16:16 -0500

    "update seed"

--- queries/songs.js
+++ queries/songs.js
@@ -1,10 +1,10 @@
 const db = require("../db/dbConfig")
 
 // async function call the data
+// db.any() function takes first str as arg,
+// .any = means it will accept any return from database
 const getAllSongs = async () => {
   try {
-    // db.any() function takes first str as arg,
-    // .any = means it will accept any return from database
     const allSongs = await db.any("SELECT * FROM songs")
     return allSongs
   } catch (error) {
