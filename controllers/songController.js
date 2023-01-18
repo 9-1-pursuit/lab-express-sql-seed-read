b49afaed5bbf78ef1b11739f109c037782e91df1
Lennie Nurse <lnursej@gmail.com>
2023-01-18 10:07:51 -0500

    "update seed"

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const songs = express.Router()
-const { getAllSongs } = require(`../queries/songs`)
+const { getAllSongs } = require("../queries/songs")
 
 // Index
 songs.get("/", async (req, res) => {
