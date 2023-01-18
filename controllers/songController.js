651e04d532a6cc1e9ccd204251d2124c6e14109b
minagrullon <minagrullon@pursuit.org>
2023-01-18 13:06:06 -0500

    part 2 complete

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs.js");
+const { getAllSongs, getSong, createSong } = require("../queries/songs.js");
+const {
+  checkName,
+  checkArtist,
+  checkFav,
+} = require("../validations/checkSongs");
 
 //index
 songs.get("/", async (req, res) => {
@@ -11,6 +16,27 @@ songs.get("/", async (req, res) => {
     res.status(500).json({ error: "Server Error" });
   }
   //   res.json({ status: "ok" });
+});
+
+//SHOW 1 song
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await getSong(id);
+  if (!song.message) {
+    res.status(200).json(song);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+//POST
+songs.post("/", checkName, checkArtist, checkFav, async (req, res) => {
+  try {
+    const newSong = await createSong(req.body);
+    res.status(200).json(newSong);
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
 });
 
 module.exports = songs;
---
+++ validations/checkSongs.js
@@ -0,0 +1,29 @@
+const checkName = (req, res, next) => {
+  if (req.body.name) {
+    next();
+  } else {
+    res.status(400).json({ error: "Name is required!" });
+  }
+};
+
+const checkArtist = (req, res, next) => {
+  if (req.body.artist) {
+    next();
+  } else {
+    res.status(400).json({ error: "Artist is required" });
+  }
+};
+
+const checkFav = (req, res, next) => {
+  if (
+    typeof req.body.is_favorite === true ||
+    typeof req.body.is_favorite === false ||
+    typeof req.body.is_favorite === undefined
+  ) {
+    next();
+  } else {
+    res.status(400).json({ error: "is_favorite should be a boolean value" });
+  }
+};
+
+module.exports = { checkName, checkArtist, checkFav };
