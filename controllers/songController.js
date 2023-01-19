dcf8171b157dd5d94e29007ca7391d195903122b
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2023-01-19 13:18:32 -0500

    part 3 done

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const songs = express.Router();
-const { getAllSongs, getSong, createSong } = require('../queries/songs');
+const {
+  getAllSongs,
+  getSong,
+  createSong,
+  deleteSong,
+  updateSong,
+} = require('../queries/songs');
 const {
   checkName,
   checkArtist,
@@ -34,6 +40,22 @@ songs.post('/', checkName, checkArtist, checkBoolean, async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: error });
   }
+});
+
+//! Delete
+
+songs.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedSong = await deleteSong(id);
+  if (deletedSong.id) res.status(200).json(deletedSong);
+  else res.status(404).json('Song not found');
+});
+
+//! Update
+songs.put('/:id', checkName, checkBoolean, checkArtist, async (req, res) => {
+  const { id } = req.params;
+  const updatedSong = await updateSong(id, req.body);
+  res.status(200).json(updatedSong);
 });
 
 module.exports = songs;
--- validations/checkSongs.js
+++ validations/checkSongs.js
@@ -21,4 +21,15 @@ const checkBoolean = (req, res, next) => {
   }
 };
 
-module.exports = { checkName, checkArtist, checkBoolean };
+const validateUrl = (req, res, next) => {
+  if (
+    req.body.url.substring(0, 7) === 'http://' ||
+    req.body.url.substring(0, 8) === 'https://'
+  )
+    return next();
+  else
+    res
+      .status(400)
+      .json({ error: 'You forgot to start your url with http:// or https://' });
+};
+module.exports = { checkName, checkArtist, checkBoolean, validateUrl };
