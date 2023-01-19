089e40391d310125c08a30148666bd779d51a412
Gillesedgy <edgygilles@pursuit.org>
2023-01-19 14:56:41 -0500

    Fixed checkTime Validation

--- Validations/checkSong.js
+++ Validations/checkSong.js
@@ -29,7 +29,7 @@ const checkBoolean = (req, res, next) => {
   // CHECK for time 
   const checkTime = (req,res, next) =>{
     const time = req.body.time
-    if (time.length >=4 ){
+    if (time.length === 4 ){
       return next()
     }else {
       res.status(404).json({erro: 'Time bust look like this 3:19'})
