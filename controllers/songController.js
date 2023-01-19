14f9c225952615cb7a6902ba8f3de07547558841
Salinamalek <salinamalek@pursuit.org>
2023-01-19 11:41:20 -0500

    part 3 done

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs, getSong, createSong } = require("../queries/songs");
+const {
+  getAllSongs,
+  getSong,
+  createSong,
+  deleteSong,
+  updateSong,
+} = require("../queries/songs");
 const {
   checkName,
   checkArtist,
   checkBoolean,
+  validateURL,
 } = require("../validations/checkSongs.js");
 
 // INDEX
@@ -29,13 +36,38 @@ songs.get("/:id", async (req, res) => {
 });
 
 // CREATE
-songs.post("/", checkName, checkArtist, checkBoolean, async (req, res) => {
-  try {
-    const song = await createSong(req.body);
-    res.json(song);
-  } catch (error) {
-    res.status(400).json({ error: error });
+songs.post(
+  "/",
+  checkName,
+  checkArtist,
+  checkBoolean,
+  validateURL,
+  async (req, res) => {
+    try {
+      const song = await createSong(req.body);
+      res.json(song);
+    } catch (error) {
+      res.status(400).json({ error: error });
+    }
   }
+);
+
+// DELETE
+songs.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedSong = await deleteSong(id);
+  if (deletedSong.id) {
+    res.status(200).json(deletedSong);
+  } else {
+    res.status(404).json("Song not found");
+  }
+});
+
+// UPDATE
+songs.put("/:id", checkName, checkBoolean, validateURL, async (req, res) => {
+  const { id } = req.params;
+  const updatedSong = await updateSong(id, req.body);
+  res.status(200).json(updatedSong);
 });
 
 module.exports = songs;
