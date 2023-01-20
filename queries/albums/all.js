ad96d2e98f68b8ad1592c14ed59fe3e3eab38df2
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-19 21:22:37 -0500

    removed song name from album object

--- queries/albums/all.js
+++ queries/albums/all.js
@@ -2,7 +2,7 @@ const database = require("../../database/databaseConfig.js")
 
 async function getAllAlbums() {
     try {
-        const allAlbums = await database.any('SELECT album_id, album_name, name AS song_name, artist, id AS song_id FROM albums LEFT JOIN songs ON album_name = album')
+        const allAlbums = await database.any('SELECT album_id, album_name, artist, id AS song_id FROM albums LEFT JOIN songs ON album_name = album')
         return allAlbums
     } catch(err) {
         return err
