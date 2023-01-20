a325271452354f011e68aa9ca2f55ba0916d3ac6
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-19 21:17:40 -0500

    added show songs from album route with query function, functioning

--- controllers/albums.js
+++ controllers/albums.js
@@ -2,10 +2,27 @@ const express = require("express")
 const router = express.Router()
 
 const { getAllAlbums } = require("../queries/albums/all.js")
+const { getOneAlbum, getSongsFromAlbum } = require("../queries/albums/show.js")
 
+// GET ALL ALBUMS
 router.get("/", async (req, resp) => {
     const albums = await getAllAlbums()
     resp.status(200).json(albums)
 })
 
+// SHOW ROUTE (ONE ALBUM)
+router.get("/:id", async (req, resp) => {
+    const {id} = req.params
+    const album = await getOneAlbum(id)
+    
+    album.album_id ? resp.json(album) : resp.status(404).json({Error: "Album id not found"})
+})
+
+// SHOW ROUTE FOR SONGS ON ALBUM
+router.get("/:id/songs", async (req, resp) => {
+    const {id} = req.params
+    const songs = await getSongsFromAlbum(id)
+
+    songs.length > 0 ? resp.status(200).json(songs) : resp.status(404).json({Error: "Album Not Found"})
+})
 module.exports = router
---
+++ queries/albums/show.js
@@ -0,0 +1,25 @@
+const database = require("../../database/databaseConfig.js")
+
+async function getOneAlbum(idValue) {
+    try {
+        const oneAlbum = await database.one('SELECT album_id, album_name, artist FROM albums LEFT JOIN songs ON album_name = album WHERE album_id = $1', idValue)
+        return oneAlbum
+    } catch(err) {
+        return err
+    }
+}
+
+// function for songs from an album -> .any() can be multiple
+async function getSongsFromAlbum(idValue) {
+    try {
+        const songs = await database.any('SELECT name FROM albums LEFT JOIN songs ON album_name = album WHERE album_id = $1', idValue)
+        return songs
+    } catch(err) {
+        return err
+    }
+}
+
+module.exports = { 
+    getOneAlbum,
+    getSongsFromAlbum, 
+}
