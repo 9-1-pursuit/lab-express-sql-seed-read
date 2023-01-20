2ac2a1d9b43d057076daaa8c4f4e8ccd6e692b66
Kylie-Lang1 <kylielang@pursuit.org>
2023-01-20 01:41:16 -0500

    11 of 14 tests passed

--- controllers/songController.js
+++ controllers/songController.js
@@ -42,19 +42,6 @@ songs.post("/", checkName, checkArtist, checkBoolean, async (req, res) => {
 });
 
 // DELETE
-// songs.delete("/:id", async (req, res) => {
-//   try{
-//     const { id } = req.params;
-//     const deletedSong = await deleteSong(id);
-//     console.log(deletedSong, deletedSong.id, id )
-//     if (deletedSong.id === id){
-//         res.status(200).json(deletedSong);
-//     }
-//   } catch (error) {
-//     res.status(404).json({error: "Song not found"});
-//   }
-// });
-
 songs.delete("/:id", async (req, res) => {
     try{
         const { id } = req.params;
