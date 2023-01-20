cbba37976c3fe0469a511dda27a4b9b762d1d152
dilrubamajumder <dilrubamajumder@pursuit.org>
2023-01-20 03:34:12 -0500

    Part 1, 2, 3

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('The crying Game', 'Nicki Minaj', 'The Pinkprint','4:25', true),
+('If the world was ending', 'JP Saxe and Julia Micheaels', 'If the world was ending','3:59', true),
+('Work', 'Rihanna', 'ANTI','3:39', true)
