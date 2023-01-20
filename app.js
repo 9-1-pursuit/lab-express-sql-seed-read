3f8d368b8ff462f98b6772c2684eb4f8e9e4cf90
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-19 21:25:45 -0500

    updated server home page with albums endpoints

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ app.use("/songs", require("./controllers/songs.js"))
 app.use("/albums", require("./controllers/albums.js"))
 
 app.get("/", (req, resp) => {
-    resp.status(200).send('<h1>Tuner App Server</h1><p>endpoints<li>/songs</li><li>/songs/:id</li></p>')
+    resp.status(200).send(
+        '<h1>Tuner App Server</h1><p>endpoints<li>/songs</li><li>/songs/:id</li><li>/albums</li><li>/albums/:id</li><li>/albums/:id/songs</li></p>'
+        )
 })
 
 app.get("*", (req, resp) => {
