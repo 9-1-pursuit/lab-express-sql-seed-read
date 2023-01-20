381a94f0f0f3a046f2ad3ff96f4e0b90ca4eed6b
chrisking718 <chrisking@pursuit.org>
2023-01-20 03:17:42 -0500

    3 test not passing but app functions correctly

---
+++ controllers/songController.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const songs = express.Router();
+const { getAllSongs, getSong, createSong, deleteSong,updateSong } = require("../queries/songs")
+const {checkName, checkBoolean} = require("../validations/checkSongs")
+
+// INDEX
+songs.get("/", async (req, res) => {
+    const allSongs = await getAllSongs();
+    if (allSongs[0]) {
+        res.status(200).json(allSongs);
+      } else {
+        res.status(500).json({ error: "server error" });
+      }
+});
+
+songs.get("/:id", async (req, res) =>{
+    const {id} = req.params
+    const song = await getSong(id)
+    if(song){
+        res.status(200).json(song)
+    } else{
+        res.status(404).json({error: "not found"})
+    }
+})
+
+songs.post("/", checkName, checkBoolean, async (req,res) => {
+    try{
+        const song = await createSong(req.body)
+        res.status(200).json(song)
+    } catch (error){
+        res.status(400).json({error: error})
+    }
+})
+
+songs.delete("/:id", async ( req, res) => {
+    const {id} = req.params
+    const deletedSong = await deleteSong(id)
+    if(deletedSong.id){
+    res.status(200).json(deletedSong)
+    }else res.status(404).json("Song not found")
+})
+
+songs.put("/:id", checkBoolean, checkName, async (req, res) => {
+    const { id } = req.params;
+    const updatedSong = await updateSong(id, req.body);
+
+        res.status(200).json(updatedSong)
+    
+    
+  });
+
+module.exports = songs;
---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+\c songs_dev
+
+INSERT INTO songs (name, artist, album, time, is_favorite ) VALUES 
+('Happy', 'Pharrell Williams', 'Happy', ' 4:00', false);
