2fe5709ac371fc290c09cc47cedcdc2ef5d9d1ae
MarsiaRochelin <marsrochelin@pursuit.org>
2023-01-19 22:32:18 -0500

    part 1

---
+++ db/dbConfig.js
@@ -0,0 +1,16 @@
+// DEPENDANCIES //
+const pgp = require("pg-promise")();
+require("dotenv").config();
+
+// CONFIGURATION //
+const cn = {
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+};
+
+const db = pgp(cn);
+
+// EXPORT //
+module.exports = db;
