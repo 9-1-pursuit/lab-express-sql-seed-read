0956c104c421b330bcaca41f24e83c612188fd81
DanielR2105 <rodriguez.daniel2105@gmail.com>
2023-01-19 19:44:05 -0500

    finished lab

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES 
+('247', 'Mike Dimes', '247', '2:43', true),
+('Bikini Bottom', 'Ice Spice', 'Bikini Bottom', '1:47', true),
+('Bruxelles je t`aime', 'Angele', 'Nonante-Cinq', '3:48', true),
+('LVL', 'A$AP ROCKY', 'LONG.LIVE.A$AP', '3:40', true),
+('Gidget', 'Free Nationals', 'Free Nationals', '3:39', true);
