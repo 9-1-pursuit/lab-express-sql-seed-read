77735da6061282b4ab3244814f27ccff79197064
DanielR2105 <rodriguez.daniel2105@gmail.com>
2023-01-23 01:33:26 -0500

    added front-end

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,55 +1,66 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs, getSong, createSong, deleteSong, updateSong } = require("../queries/songs")
-const { checkName, checkArtist, checkBoolean, validateURL } = require("../validations/checkSongs")
+const {
+  getAllSongs,
+  getSong,
+  createSong,
+  deleteSong,
+  updateSong,
+} = require("../queries/songs");
+const {
+  checkName,
+  checkArtist,
+  checkBoolean,
+  validateURL,
+} = require("../validations/checkSongs");
 
 // INDEX
 songs.get("/", async (req, res) => {
-    const allSongs = await getAllSongs();
-    if (allSongs[0]) {
-      res.status(200).json(allSongs);
-    } else {
-      res.status(500).json({ error: "No songs found" });
-    }
-  });
+  const allSongs = await getAllSongs();
+  if (allSongs[0]) {
+    res.status(200).json(allSongs);
+  } else {
+    res.status(500).json({ error: "No songs found" });
+  }
+});
 
-  // SHOW
+// SHOW
 songs.get("/:id", async (req, res) => {
-    const { id } = req.params;
-    const song = await getSong(id);
-    if (song) {
-      res.json(song);
-    } else {
-      res.status(404).json({ error: "Song not found" });
-    }
-  });
+  const { id } = req.params;
+  const song = await getSong(id);
+  if (song) {
+    res.json(song);
+  } else {
+    res.status(404).json({ error: "Song not found" });
+  }
+});
 
-  // CREATE
-songs.post("/", checkName, checkArtist, checkBoolean, validateURL, async (req, res) => {
-    try {
-      const song = await createSong(req.body);
-      res.json(song);
-    } catch (error) {
-      res.status(400).json({ error: error });
-    }
-  });
+// CREATE
+songs.post("/", checkName, checkArtist, checkBoolean, async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    res.json(song);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
 
-  // DELETE
-  songs.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    const deletedSong = await deleteSong(id);
-    if (deletedSong.id) {
-      res.status(200).json(deletedSong);
-    } else {
-      res.status(404).json("Song not found");
-    }
-  });
+// DELETE
+songs.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedSong = await deleteSong(id);
+  if (deletedSong.id) {
+    res.status(200).json(deletedSong);
+  } else {
+    res.status(404).json("Song not found");
+  }
+});
 
-  // UPDATE
-songs.put("/:id", checkArtist, checkBoolean, checkName, validateURL, async (req, res) => {
-    const { id } = req.params;
-    const updatedSong = await updateSong(id, req.body);
-    res.status(200).json(updatedSong);
-  });
+// UPDATE
+songs.put("/:id", checkArtist, checkBoolean, checkName, async (req, res) => {
+  const { id } = req.params;
+  const updatedSong = await updateSong(id, req.body);
+  res.status(200).json(updatedSong);
+});
 
 module.exports = songs;
