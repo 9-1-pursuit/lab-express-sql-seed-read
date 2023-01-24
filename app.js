f59684a8973762e306870fdcf20dd5d7a26bcb94
Richie-Pursuit <richiecarrasco@pursuit.org>
2023-01-24 02:20:40 -0500

    part 4 done (added playlist)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const cors = require('cors');
 const express = require('express');
 const songsController = require('./controllers/songController.js');
+const playlistController = require("./controllers/playlistController.js");
 
 // Congfig
 const app = express();
@@ -11,6 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/songs', songsController);
+app.use("/playlists", playlistController);
+
 
 // Routes
 
--- db/seed.sql
+++ db/seed.sql
@@ -5,3 +5,12 @@ INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
 ('Hit em up', 'Tupac', 'Death Row Greatest Hit','3:22', true), 
 ('Bikini Bottom', 'Ice Spice', 'Bikini Bottom', '1:47', true), 
 ('Sacrifice', 'The Weeknd', 'Dawn FM', '3:44', true);
+
+INSERT INTO playlists (name, description, song_id)
+VALUES
+('Hype-Hop', 'Blazing Hip-Hop & R&B', 1),
+('Gym Life', 'Top 40 Hottest songs for the gym', 2),
+('2022', 'The best song of the year 2022', 3);
+
+
+--run command: psql -U postgres -f db/seed.sql
