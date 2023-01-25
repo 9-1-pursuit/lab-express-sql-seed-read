575366ff405729ef625c10127ce203a2cbb98c01
Lennie Nurse <lnursej@gmail.com>
2023-01-25 14:52:05 -0500

    "updated seed.sql"

--- db/seed.sql
+++ db/seed.sql
@@ -8,6 +8,19 @@ INSERT INTO playlist (title, genre) VALUES
 ('Playlist for Her', 'mix genre'),
 ('AfroBeats mix', 'mix');
 
+INSERT INTO artist (name, album) VALUES 
+('Tems', 'For Broken Ears'),
+('Kendrick Lamar', 'Mr. Morale & The Big Steppers'),
+('Future', 'I NEVER LIKED YOU'),
+('Brent Faiyaz', 'Fuck The World'),
+('Jay-z', 'Reasonable Doubt'),
+('Micheal Jackson', 'Thriller'),
+('lil Wayne', 'Tha Carter II');
+
+-- INSERT INTO album (name, artist, released) VALUES
+-- ()
+
+
 
 INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
 ('Billy Jean', 'Micheal Jackson', 'Thriller', '4:55', true),
@@ -16,4 +29,17 @@ INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
 ('Free', 'Carson', 'NO ', '1:55', false),
 ('Dead Presidents', 'Jay-z', 'Reasonable Doubt', '4:26', true),
 ('All I NEED', 'Popcaan', 'Fixtape', '2:58', true),
-('Count Me Out', 'kendrick Lamar', 'Mr. Morale & The Big Steppers', '4:43', true);
+('Count Me Out', 'kendrick Lamar', 'Mr. Morale & The Big Steppers', '4:43', true),
+('Clouded', 'Brent Faiyaz', 'Fuck The World', '1:50', true),
+('Exchange', 'Bryson Tiller', 'TRAPSOUL', '3:14', true),
+('Higher', 'Tems', 'For Broken Ears', '3:16', true),
+('WAIT FOR U', 'Future', 'I NEVER LIKED YOU', '3:09', true),
+('Damages', 'Tems', 'For Broken Ears', '2:49', true),
+('LOVE YOU BETTER', 'Future', 'I NEVER LIKED YOU', '2:09', true),
+('ALL DAY', 'Kanye WEST', 'single', '5:10', true),
+('City of Gods', 'Fivio Foreign', 'B.I.B.L.E', '4:16', true),
+('Otis', 'Jay-z Kanye West', 'Watch throne', '2:58', true),
+('understand', 'Omah Lay', 'Boy Alone', '2:54', true),
+('Rush', 'Ayra Starr', '19& Dangerous(DELUXE)', '3:05', true)
+('23', 'Skang', 'single', '2:24', true),
+('More Life', 'Burna Boy', 'OUTSIDE', '1:22', false);
