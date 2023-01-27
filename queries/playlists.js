33fe966dee65e42a5090be6ac1d1e8669a6abbe3
Lennie Nurse <lnursej@gmail.com>
2023-01-27 10:19:32 -0500

    "update"

--- queries/playlists.js
+++ queries/playlists.js
@@ -1,22 +1,30 @@
 const db = require("../db/dbConfig")
 
 // Index playlist
-const getAllPlaylist = async (playlistId) => {
-  try {
-    const allPlaylist = await db.any(
-      "SELECT * FROM playlist WHERE songs_id=$1",
-      playlistId
-    )
-    return allPlaylist
-  } catch (error) {
-    return error
+const getAllPlaylist = async (songs_id) => {
+  if (songs_id === true) {
+    try {
+      const allPlaylist = await db.any(
+        "SELECT * FROM playlists WHERE songs_id=$1",
+        playlistId
+      )
+      return allPlaylist
+    } catch (error) {
+      return error
+    }
+  } else {
+    try {
+      const allPlaylist = await db.any("SELECT * FROM playlists ")
+    } catch (error) {
+      return error
+    }
   }
 }
 
 // get one playlist
 const getOnePlaylist = async (id) => {
   try {
-    const onePlaylist = await db.one("SELECT * FROM playlist WHERE id=$1", id)
+    const onePlaylist = await db.one("SELECT * FROM playlists WHERE id=$1", id)
     return onePlaylist
   } catch (error) {
     return error
@@ -27,20 +35,20 @@ const getOnePlaylist = async (id) => {
 const createPlaylist = async (playlist) => {
   try {
     const createdOnePlaylist = await db.one(
-      "INSERT INTO playlist(title, genre) VALUES($1, $2) RETURNING *",
-      [playlist.title, playlist.genre]
+      "INSERT INTO playlists(title, genre) VALUES($1, $2) RETURNING *",
+      [playlist.title, playlist.genre, songs_id]
     )
     return createdOnePlaylist
   } catch (error) {
     return error
   }
 }
-
+s
 // delete a playlist
 const deletePlaylist = async (id) => {
   try {
     const deletedPlaylist = await db.one(
-      "DELETE FROM playlist WHERE id=$1 RETURNING * ",
+      "DELETE FROM playlists WHERE id=$1 RETURNING * ",
       id
     )
     return deletedPlaylist
@@ -53,7 +61,7 @@ const deletePlaylist = async (id) => {
 const updatePlaylist = async (id, playlist) => {
   try {
     const updatedPlaylist = await db.one(
-      "UPDATE playlist SET title=$1, genre=$2 WHERE id=$3 RETURNING *",
+      "UPDATE playlists SET title=$1, genre=$2 WHERE id=$3 RETURNING *",
       [playlist.title, playlist.genre, id]
     )
     return updatedPlaylist
