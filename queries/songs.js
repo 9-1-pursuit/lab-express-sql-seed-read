b48f419bfd38aef64558af4355b57bc36d3c922c
minagrullon <minagrullon@pursuit.org>
2023-01-26 22:44:40 -0500

    post for songs was missing id

--- queries/songs.js
+++ queries/songs.js
@@ -26,8 +26,15 @@ const getSong = async (id) => {
 const createSong = async (song) => {
   try {
     const newSong = await db.one(
-      "INSERT INTO songs(name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
-      [song.name, song.artist, song.album, song.time, song.is_favorite]
+      "INSERT INTO songs(name, artist, album, time, is_favorite, playlist_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+      [
+        song.name,
+        song.artist,
+        song.album,
+        song.time,
+        song.is_favorite,
+        song.playlist_id,
+      ]
     );
     return newSong;
   } catch (error) {
