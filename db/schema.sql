82c1c419fa02fd4594604314da11be842dfd5fe0
Lennie Nurse <lnursej@gmail.com>
2023-01-27 11:08:57 -0500

    "update"

--- db/schema.sql
+++ db/schema.sql
@@ -38,17 +38,7 @@ CREATE TABLE songs (
 
 );
 
-INSERT INTO reviews (songs_id, reviewer, title, content, rating )
-VALUES
-('1', 'Nick', 'My Favorite', 'This website crushes it when it comes to awesome explanations', 3),
-('2', 'James', 'My Favorite', 'This website crushes it when it comes to inspiring me', 3),
-('3', 'Tisha', 'My Least Favorite', 'This website crushes it when it comes to destroying my patience', 5),
-('2', 'Juliana', 'I Love Going Here', 'I finally learned how to properly fold a fitted sheet', 5),
-('2', 'Dave', 'Cool Site', 'But I got way into adding decorative pillows everywhere', 1),
-('2', 'Mr. Cool', 'So Helpful', 'I got some awesome recommendations for a ceiling fan and some spoons', 3),
-('2', 'Alison', 'A lifesaver!','Helped me get my stove cleaner than I ever imagiend possible!', 4),
-('3', 'Hannah', 'Insert Confetti Emoji Here', 'I survived 6 hours at the DMV!', 4),
-('3', 'Gabi', 'My Friend Hannah', 'Gets a discount if I leave a positive review, so here it is', 5);
+
 -- will figure out how to use these later
     -- playlist_id INTEGER REFERENCES playlist (id) ON DELETE CASCADE
     -- album_id INTEGER REFERENCES album (id) ON DELETE CASCADE
--- db/seed.sql
+++ db/seed.sql
@@ -52,3 +52,15 @@ INSERT INTO songs (name, artist, album, time, is_favorite, artists_id) VALUES
 ('Rush', 'Ayra Starr', '19& Dangerous(DELUXE)', '3:05', true, 18),
 ('23', 'Skang', 'single', '2:24', true, 19),
 ('More Life', 'Burna Boy', 'OUTSIDE', '1:22', false, 20);
+
+INSERT INTO reviews (songs_id, reviewer, title, content, rating )
+VALUES
+('1', 'Nick', 'My Favorite', 'This website crushes it when it comes to awesome explanations', 3),
+('2', 'James', 'My Favorite', 'This website crushes it when it comes to inspiring me', 3),
+('3', 'Tisha', 'My Least Favorite', 'This website crushes it when it comes to destroying my patience', 5),
+('2', 'Juliana', 'I Love Going Here', 'I finally learned how to properly fold a fitted sheet', 5),
+('2', 'Dave', 'Cool Site', 'But I got way into adding decorative pillows everywhere', 1),
+('2', 'Mr. Cool', 'So Helpful', 'I got some awesome recommendations for a ceiling fan and some spoons', 3),
+('2', 'Alison', 'A lifesaver!','Helped me get my stove cleaner than I ever imagiend possible!', 4),
+('3', 'Hannah', 'Insert Confetti Emoji Here', 'I survived 6 hours at the DMV!', 4),
+('3', 'Gabi', 'My Friend Hannah', 'Gets a discount if I leave a positive review, so here it is', 5);
