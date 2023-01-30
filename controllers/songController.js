c77c5b44381dd6a6b847575b05f7741406148c5c
melanieinpursuit <melaniegonzalez@pursuit.org>
2023-01-30 15:39:08 -0500

    added all routes and got them working

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require('../queries/songs.js')
+const { getAllSongs, getSong, createSong, deleteSong, updateSong } = require('../queries/songs.js')
 
 // INDEX
 songs.get("/", async (req, res) => {
@@ -11,5 +11,48 @@ songs.get("/", async (req, res) => {
     res.status(500).json({ error: "server error" });
   }
 });
+
+// GET ONE SONG / SHOW
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params
+  const song = await getSong(id)
+  if (song) {
+    res.json(song)
+  } else {
+    res.status(404).json({ error: "not found"})
+  }
+})
+
+// ADD SONG
+songs.post("/", async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    res.json(song);
+  } catch (error) {
+    res.status(400).json({ error: error})
+  }
+})
+
+// DELETE SONG
+songs.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedSong = await deleteSong(id)
+    res.status(200).json(deletedSong)
+  } catch (error) {
+    res.status(404).json({ error: "id not found" })
+  }
+})
+
+// UPDATE SONG
+songs.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const updatedSong = await updateSong(id, req.body)
+    res.status(200).json(updatedSong)
+  } catch (error) {
+    res.status(404).json({ error: "song not found" })
+  }
+})
 
 module.exports = songs;
