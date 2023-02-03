ba630e4aada8a61d4a5a3b28fe7e2841265ff6d0
DestinyJoyner <destinyjoyner@pursuit.org>
2023-02-03 01:38:28 -0500

    deploying

--- database/databaseConfig.js
+++ database/databaseConfig.js
@@ -6,12 +6,22 @@ require("dotenv").config()
 
 
 // Connection Object, use values from .env file
-const cn = {
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-    user: process.env.PG_USER,
-}
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
+// https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax#configuration-object
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER,
+    };
 
 // open the connection with const db = pgp(cn);
 const database = pgp(cn)
